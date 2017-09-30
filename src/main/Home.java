9ec64df21166e0391b476189001e73698d3e1c16
bhm699 <bhm699@cs.bham.ac.uk>
2017-09-30 16:15:28 +0100

    add homepage (in src/main)

---
+++ src/main/Home.java
@@ -0,0 +1,130 @@
+package main;
+
+/**
+ *
+ * @author bhavi
+ */
+public class Home extends javax.swing.JFrame {
+
+	private javax.swing.JButton chooseSolitare;
+	private javax.swing.JLabel jLabel1;
+	private javax.swing.JMenu jMenu1;
+	private javax.swing.JMenu jMenu2;
+	private javax.swing.JMenuBar jMenuBar1;
+	private javax.swing.JMenuItem jMenuItem1;
+	private javax.swing.JPanel jPanel1;
+
+	public Home() {
+		initComponents();
+	}
+
+	private void initComponents() {
+		jPanel1 = new javax.swing.JPanel();
+		jLabel1 = new javax.swing.JLabel();
+		chooseSolitare = new javax.swing.JButton();
+		jMenuBar1 = new javax.swing.JMenuBar();
+		jMenu1 = new javax.swing.JMenu();
+		jMenuItem1 = new javax.swing.JMenuItem();
+		jMenu2 = new javax.swing.JMenu();
+
+		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+		jLabel1.setFont(new java.awt.Font("Copperplate Gothic Light", 0, 14)); // NOI18N
+		jLabel1.setText("WELCOME. Please choose a card game to play.");
+
+		chooseSolitare.setIcon(new javax.swing.ImageIcon("img/buttons/solitare.gif")); // NOI18N
+		chooseSolitare.setText("SOLITARE");
+		chooseSolitare.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+		chooseSolitare.setActionCommand("Solitare");
+		chooseSolitare.setBackground(new java.awt.Color(255, 255, 255));
+		chooseSolitare.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				chooseSolitareActionPerformed(evt);
+			}
+		});
+
+		javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+		jPanel1.setLayout(jPanel1Layout);
+		jPanel1Layout
+				.setHorizontalGroup(
+						jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+								.addGroup(jPanel1Layout.createSequentialGroup().addGap(25, 25, 25)
+										.addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 429,
+												Short.MAX_VALUE)
+										.addContainerGap())
+								.addGroup(jPanel1Layout.createSequentialGroup().addContainerGap()
+										.addComponent(chooseSolitare)
+										.addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
+		jPanel1Layout.setVerticalGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+				.addGroup(jPanel1Layout.createSequentialGroup().addContainerGap()
+						.addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 50,
+								javax.swing.GroupLayout.PREFERRED_SIZE)
+						.addGap(18, 18, 18).addComponent(chooseSolitare).addContainerGap(120, Short.MAX_VALUE)));
+
+		jMenu1.setText("File");
+
+		jMenuItem1.setText("Exit");
+		jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				jMenuItem1ActionPerformed(evt);
+			}
+		});
+		jMenu1.add(jMenuItem1);
+
+		jMenuBar1.add(jMenu1);
+
+		jMenu2.setText("Edit");
+		jMenuBar1.add(jMenu2);
+
+		setJMenuBar(jMenuBar1);
+
+		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+		getContentPane().setLayout(layout);
+		layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addComponent(
+				jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
+		layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addComponent(
+				jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
+
+		pack();
+	}// </editor-fold>
+
+	private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {
+		System.exit(0);
+	}
+
+	private void chooseSolitareActionPerformed(java.awt.event.ActionEvent evt) {
+		solitare.Main.main(null);
+	}
+
+
+	public static void main(String args[]) {
+		/* Set the Nimbus look and feel */
+		// <editor-fold defaultstate="collapsed" desc=" Look and feel setting
+		// code (optional) ">
+		/*
+		 * If Nimbus (introduced in Java SE 6) is not available, stay with the
+		 * default look and feel. For details see
+		 * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.
+		 * html
+		 */
+		try {
+			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (ClassNotFoundException ex) {			
+		} catch (InstantiationException ex) {			
+		} catch (IllegalAccessException ex) {			
+		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+		}
+
+		// Create and display the form
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new Home().setVisible(true);
+			}
+		});
+	}
+}
