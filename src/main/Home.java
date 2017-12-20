ae61a63343f6e1c1dadc464cfff73f255fa62d75
bhm699 <bhm699@cs.bham.ac.uk>
2017-12-20 22:28:36 +0000

    hardcoded imports removed

--- src/main/Home.java
+++ src/main/Home.java
@@ -1,69 +1,71 @@
 package main;
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 /**
  *
  * @author bhavi
  */
 public class Home extends javax.swing.JFrame {
 
-	private javax.swing.JButton chooseSolitare;
-	private javax.swing.JLabel jLabel1;
-	private javax.swing.JMenu jMenu1;
-	private javax.swing.JMenuBar jMenuBar1;
-	private javax.swing.JMenuItem jMenuItem1;
-	private javax.swing.JPanel jPanel1;
+	private JButton chooseSolitare;
+	private JLabel jLabel1;
+	private JMenu jMenu1;
+	private JMenuBar jMenuBar1;
+	private JMenuItem jMenuItem1;
+	private JPanel jPanel1;
 
 	public Home() {
 		initComponents();
 	}
 
 	private void initComponents() {
-		jPanel1 = new javax.swing.JPanel();
-		jLabel1 = new javax.swing.JLabel();
-		chooseSolitare = new javax.swing.JButton();
-		jMenuBar1 = new javax.swing.JMenuBar();
-		jMenu1 = new javax.swing.JMenu();
-		jMenuItem1 = new javax.swing.JMenuItem();
+		jPanel1 = new JPanel();
+		jLabel1 = new JLabel();
+		chooseSolitare = new JButton();
+		jMenuBar1 = new JMenuBar();
+		jMenu1 = new JMenu();
+		jMenuItem1 = new JMenuItem();
 		
-		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-		jLabel1.setFont(new java.awt.Font("Copperplate Gothic Light", 0, 16)); 
+		jLabel1.setFont(new Font("Copperplate Gothic Light", 0, 16)); 
 		jLabel1.setText("WELCOME");
 		jLabel1.setText("<html>WELCOME<br/>Please choose a card game to play</html>");
 
-		chooseSolitare.setIcon(new javax.swing.ImageIcon("img/buttons/solitare.gif"));
-		chooseSolitare.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+		chooseSolitare.setIcon(new ImageIcon("img/buttons/solitare.gif"));
+		chooseSolitare.setCursor(new Cursor(java.awt.Cursor.HAND_CURSOR));
 		chooseSolitare.setActionCommand("Solitare");
-		chooseSolitare.setBackground(new java.awt.Color(0, 0, 0));
-		chooseSolitare.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
+		chooseSolitare.setBackground(new Color(0, 0, 0));
+		chooseSolitare.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
 				chooseSolitareActionPerformed(evt);
 			}
 		});
 
-		javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+		GroupLayout jPanel1Layout = new GroupLayout(jPanel1);
 		jPanel1.setLayout(jPanel1Layout);
 		jPanel1Layout
 				.setHorizontalGroup(
-						jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+						jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
 								.addGroup(jPanel1Layout.createSequentialGroup().addGap(25, 25, 25)
-										.addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 429,
+										.addComponent(jLabel1, GroupLayout.DEFAULT_SIZE, 429,
 												Short.MAX_VALUE)
 										.addContainerGap())
 								.addGroup(jPanel1Layout.createSequentialGroup().addContainerGap()
 										.addComponent(chooseSolitare)
-										.addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
-		jPanel1Layout.setVerticalGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+										.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
+		jPanel1Layout.setVerticalGroup(jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
 				.addGroup(jPanel1Layout.createSequentialGroup().addContainerGap()
-						.addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 50,
-								javax.swing.GroupLayout.PREFERRED_SIZE)
+						.addComponent(jLabel1, GroupLayout.PREFERRED_SIZE, 50,
+								GroupLayout.PREFERRED_SIZE)
 						.addGap(18, 18, 18).addComponent(chooseSolitare).addContainerGap(120, Short.MAX_VALUE)));
 
 		jMenu1.setText("File");
 
 		jMenuItem1.setText("Exit");
-		jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
+		jMenuItem1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
 				jMenuItem1ActionPerformed(evt);
 			}
 		});
@@ -71,21 +73,21 @@ public class Home extends javax.swing.JFrame {
 		jMenuBar1.add(jMenu1);
 
 		
-		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+		GroupLayout layout = new GroupLayout(getContentPane());
 		getContentPane().setLayout(layout);
-		layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addComponent(
-				jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
-		layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addComponent(
-				jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
+		layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING).addComponent(
+				jPanel1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
+		layout.setVerticalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING).addComponent(
+				jPanel1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
 
 		pack();
 	}
 
-	private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {
+	private void jMenuItem1ActionPerformed(ActionEvent evt) {
 		System.exit(0);
 	}
 
-	private void chooseSolitareActionPerformed(java.awt.event.ActionEvent evt) {
+	private void chooseSolitareActionPerformed(ActionEvent evt) {
 		solitare.RunSolitare.main(null);
 	}
 
@@ -101,16 +103,16 @@ public class Home extends javax.swing.JFrame {
 		 * html
 		 */
 		try {
-			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+			for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
 				if ("Nimbus".equals(info.getName())) {
-					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					UIManager.setLookAndFeel(info.getClassName());
 					break;
 				}
 			}
 		} catch (ClassNotFoundException ex) {			
 		} catch (InstantiationException ex) {			
 		} catch (IllegalAccessException ex) {			
-		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+		} catch (UnsupportedLookAndFeelException ex) {
 		}
 
 		// Create and display the form
